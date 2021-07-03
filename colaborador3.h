78697baf05ce3d9a3b51b7024c8433c94aed853d
moisesmanzaba09 <86240688+moisesmanzaba09@users.noreply.github.com>
2021-07-03 15:50:00 -0500

    Update colaborador3.h

--- colaborador3.h
+++ colaborador3.h
@@ -2,3 +2,64 @@
 void potencia(float (*m1)[5], int p, float (*r)[5])
 {
   //INGRESE EL CODIGO PARA REALIZAR LA POTENCIA DE LA PRIMERA MATRIZ
+#include <iostream>
+
+#include <cstdlib>
+
+
+
+using namespace std;
+
+
+
+    int base;
+
+    int exponente;
+
+    int resultado=0;
+
+
+
+int main(){
+
+
+
+    cout << "Este programa le permite calcular potencias." << endl;
+
+    cout << "Ingrese la base ";
+
+    cin >> base;
+
+    cout << "Ingrese el exponente ";
+
+    cin >> exponente;
+
+int x=1;
+
+while (x<exponente){
+
+if (resultado<base){
+
+resultado=base*base;
+
+}
+
+else{
+
+resultado=resultado*base;
+
+}
+
+x++;
+
+}
+
+cout << "El resultado es: " << resultado << endl;
+
+
+
+system("pause");
+
+return 0;
+
+}
