d27fe66bec7ab490ff6da48e3270c9a8c69ad132
GarciaMelanie31 <86171662+GarciaMelanie31@users.noreply.github.com>
2021-07-04 15:16:21 -0500

    Update colaborador4.h
    Cambie algunas lineas y variables para que coincidieran con el archivo principal.

--- colaborador4.h
+++ colaborador4.h
@@ -1,28 +1,11 @@
 //COLABORADOR4 - MUSO LLUMIQUINGA CESAR GEOVANNY
 void transpuesta(float (*m1)[5], float (*r)[5])
 {
-    int f,c;
-    
-    for(int i=0;i<f;i++){
-      for(int j=0;j<c;j++){
-        cout<<"Digite un numero ["<<i<<"]["<<j<<"]:";
-        cin>>m1[i][j];
+    for(int i=0;i<5;i++){
+      for(int j=0;j<5;j++){
+        r[i][j]=m1[j][i];
+        cout<<r[j][i]<<" ";
       }
+        cout<<endl;
     }
-    
-    cout<<"\nMatriz Original\n";
-    for(int i=0;i<f;i++){
-      for(int j=0;j<c;j++){
-        cout<<m1[i][j]<<" ";
-      }
-      cout<<"\n";
-    }
-    
-    cout<<"\nMatriz  Transpuesta\n";
-    for(int i=0;i<f;i++){
-      for(int j=0;j<c;j++){
-        cout<<r[i][j]<<" ";
-      }
-      cout<<"\n";
-    }
-  }
+}
